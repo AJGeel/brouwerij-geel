3128e95383baaada105a7e1a8bd37c0d6616bb80
AJGeel <geelaj01@gmail.com>
2020-09-07 12:53:44 +0200

    Add scroll fix when transitioning pages

--- js/util.js
+++ js/util.js
@@ -434,12 +434,17 @@ Other functions
 */
 
 function scrollToTop() {
-  console.log('scroll to top');
 
+  // SWUP Animation duration: {opacity: .3s} {transform: .4s}
+  const htmlDOM = document.querySelector('html');
+
+  // Temporarily remove smoothscroll behavior
+  htmlDOM.style.scrollBehavior = 'auto';
+
+  // Reset the scroll amount
   document.body.scrollTop = 0; // Safari
   document.documentElement.scrollTop = 0; // Chrome, FireFox, IE
 
-  setTimeout(function() {
-
-  }, 1);
+  // Reintroduce the smooth scroll behavior
+  htmlDOM.style.scrollBehavior = 'smooth';
 }
