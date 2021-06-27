e12b81fb959a60aa57d9088253669ed73deea3c9
Arthur Geel <geelaj01@gmail.com>
2021-06-27 12:37:03 +0200

    Add nav visual tweaks

--- recipes/hefeweizen.php
+++ recipes/hefeweizen.php
@@ -204,7 +204,7 @@
 button.call-to-action:focus,button.call-to-action:hover{
     box-shadow:0 0 10px hsla(22,80%,60%,.5),0 0 30px hsla(22,80%,60%,.25)
 }
-nav{
+nav .navbar{
     box-shadow:0 1px 1px hsla(22,20%,60%,.08),0 2px 2px hsla(22,20%,60%,.12),0 4px 4px hsla(22,20%,60%,.16),0 8px 8px hsla(22,20%,60%,.2)
 }
 footer .social:hover{
