fc927d7d63693a2d7db9185749df7954a3ca0565
Arthur Geel <geelaj01@gmail.com>
2021-11-04 16:30:58 +0100

    Replace ascii-biertje.php component

--- 404.php
+++ 404.php
@@ -13,7 +13,32 @@
 <link rel="stylesheet" href="css/reset.min.css">
 <link rel="stylesheet" href="css/style.min.css">
 
-    <?php include 'components/ascii-biertje.php'; ?>
+    <!--
+
+            ██████████████  ██
+          ██              ██  ██
+        ██                    ██
+        ██              ████    ████              Checking out the source code, are we?
+        ██      ████████░░░░████    ██
+        ██    ██░░░░░░░░░░░░██▒▒▒▒▒▒  ██          That's cool. Have a virtual beer on the house.
+          ████░░░░░░░░░░░░░░██▒▒██▒▒  ██
+          ██░░░░░░░░░░░░░░░░████  ██  ██          < — — 
+          ██░░░░░░░░░░░░░░░░██    ██  ██
+          ██░░░░░░░░░░░░░░░░██    ██  ██
+          ██░░░░░░░░░░░░░░░░██    ██  ██
+          ██░░░░░░░░░░░░░░░░████  ██  ██          This website was designed and developed by
+          ██░░░░░░░░░░░░░░░░██  ██▒▒  ██
+          ██░░░░░░░░░░░░░░░░██  ▒▒▒▒██            Arthur Geel. If you have any questions or comments
+          ██░░░░░░░░░░░░░░░░██▒▒▒▒██
+          ██░░░░░░░░░░░░░░░░██████                regarding web design/dev and brewing, feel free to @me,
+        ██  ██░░░░░░░░░░░░░░██
+        ██  ▒▒██████████████▒▒██                  you can do so through hello@arthurgeel.com
+          ██  ▒▒▒▒        ▒▒  ██
+          ████          ████
+              ██████████
+
+-->
+
 
   </head>
   <body>
