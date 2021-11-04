6d68d4ac5df9c722f6cec56350fb99c432f6d3ff
Arthur Geel <geelaj01@gmail.com>
2021-11-04 16:40:22 +0100

    Replace footer.php component

--- recipes/placeholder.php
+++ recipes/placeholder.php
@@ -302,7 +302,55 @@
       </div>
 
       <?php include('../other-brews.php'); ?>
-      <?php include('../footer.php'); ?>
+      <!-- FOOTER COMPONENT   —   TODO: EXTRACT THIS -->
+<footer>
+  <div class="footer-inner limit-width">
+    <a class="footer-logo" href="/index.html">
+      <svg id="brouwerij-geel-logo-footer" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><defs><style>.cls-1{fill:#231f20;}</style></defs><title>brouwerij-geel-logo</title><path class="cls-1" d="M-.49,50.1H10.06c4.92,0,8.24.83,10.41,2.56a11.11,11.11,0,0,1,3.84,8.69,11.92,11.92,0,0,1-1.6,6.08A9.28,9.28,0,0,1,19.64,70c2.88.64,4.22,1.28,5.56,2.69a11.52,11.52,0,0,1,3.07,8,11.68,11.68,0,0,1-3.77,8.76c-2.49,2.24-5.88,3.26-10.61,3.26H-.49ZM10.18,67.87c4.29,0,6.46-1.85,6.46-5.56s-2.17-5.5-6.33-5.5H7.82V67.87ZM11.4,86c5.62,0,8.31-1.92,8.31-5.88a5.35,5.35,0,0,0-2.49-4.74c-1.47-1-2.82-1.21-6.27-1.21H7.82V86Z"/><path class="cls-1" d="M70.5,92.74H60.28L48.77,75.61H47.68V92.74H39.37V50.1H51.71c4.73,0,8.5,1,10.87,3a13.2,13.2,0,0,1,.38,19,11.4,11.4,0,0,1-5.88,2.69Zm-20.39-23c5.24,0,8.12-2.3,8.12-6.52s-2.75-6.4-8-6.4H47.68V69.73Z"/><path class="cls-1" d="M121.86,71.45c0,12.6-10,22.25-23,22.25A22.16,22.16,0,0,1,76.29,71.13c0-12.14,10.23-22,22.88-22S121.86,58.92,121.86,71.45Zm-37-.19c0,8.24,6.33,14.83,14.26,14.83A14.68,14.68,0,1,0,84.85,71.26Z"/><path class="cls-1" d="M141.21,74.52c0,4.35.25,5.75,1.34,7.61a9,9,0,0,0,15,0c1.08-1.86,1.34-3.26,1.34-7.61V50.1h8.31V76.18c0,5-1,8.5-3.13,11.38-2.94,3.84-8.19,6.14-14,6.14S139,91.4,136,87.56c-2.17-2.88-3.13-6.39-3.13-11.38V50.1h8.31Z"/><path class="cls-1" d="M56.64,135.93l10.42-29.41H71.6l9.9,29.41,10.23-29h8.76l-16,43.47H78.63L69.17,122.5l-10,27.87H53.38L38.29,106.9h8.76Z"/><path class="cls-1" d="M114.4,114.13v9.46h14.7v7.22H114.4v11.51h15.28v7.22H106.09V106.9h23.59v7.23Z"/><path class="cls-1" d="M170.2,149.54H160l-11.5-17.13h-1.09v17.13h-8.31V106.9h12.34c4.73,0,8.5,1,10.87,3a13.21,13.21,0,0,1,.38,19,11.48,11.48,0,0,1-5.88,2.68Zm-20.39-23c5.24,0,8.12-2.3,8.12-6.52s-2.75-6.39-8-6.39h-2.56v12.91Z"/><path class="cls-1" d="M184.46,132.73h-8.31V106.9h8.31Z"/><path class="cls-1" d="M210.73,134.41c0,5.88-1.15,9.46-3.77,12a12,12,0,0,1-8,2.94H176.15V141.6l22,0a3.81,3.81,0,0,0,3.71-2.56c.45-1.22.57-2.43.57-6V106.9h8.31Z"/><path class="cls-1" d="M143.64,183.53c-.07,6.26-.58,9.39-2.24,12.65-3.52,7.16-10.1,11.13-18.54,11.13-12.91,0-22.37-9.28-22.37-22.06,0-13,9.52-22.5,22.63-22.5,7.92,0,14.25,3.45,18.28,10l-7.29,3.9a12.23,12.23,0,0,0-11.06-6.32c-8,0-14,6.32-14,14.89,0,8.31,6,14.51,13.94,14.51,6.07,0,10.55-3.58,11.19-9h-10v-7.22Z"/><path class="cls-1" d="M163.91,170.93v9.46h17.27v7.23H163.91v11.5h17.85v7.23H155.6V163.71h26.16v7.22Z"/><path class="cls-1" d="M202,170.93v9.46H219.3v7.23H202v11.5h17.85v7.23H193.72V163.71h26.16v7.22Z"/><path class="cls-1" d="M240.35,199.12H256v7.23H232V163.71h8.31Z"/></svg>
+    </a>
+    <div class="column main">
+      <h3>About Us</h3>
+      <p>Brouwerij Geel is an independent nano-brewery based in Utrecht, the Netherlands. We have been brewing ciders & beers since 2017, and we occasionally dabble in wine-making too!</p>
+    </div>
+    <div class="column">
+      <h3>Quick Links</h3>
+      <ul>
+        <li><a class="textlink" href="../about-us.html">Our Story</a></li>
+        <li><a class="textlink" href="../brewing-archive.html">Brewing Archive</a></li>
+        <li><a class="textlink" href="../articles/index.html">Articles &amp; Thoughts</a></li>
+        <li><a class="textlink" href="../tools/index.html">Brewing Tools</a></li>
+        <li><a class="textlink" href="../privacy.html">Privacy</a></li>
+      </ul>
+    </div>
+    <div class="column">
+      <h3>Follow Us</h3>
+      <div class="inline">
+
+        <!-- <label for="h1_c2" aria-label="Minor usability problem: fixing this should be given low priority." data-balloon-pos="down" data-balloon-length="medium">2</label> -->
+
+        <a class="social" href="https://untappd.com/brouwerijgeel" target="_blank" aria-label="Our brews are on Untapped! Check out @BrouwerijGeel." data-balloon-pos="down" data-balloon-length="medium">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="arthurIcons arthur-bottle">
+            <g clip-path="url(#clip0)">
+              <path d="M8 12.297C8 11.7161 8.25249 11.164 8.69183 10.7841L9.10356 10.4281C9.49535 10.0893 9.74043 9.61175 9.78722 9.09592L10.4349 1.95483C10.4583 1.69725 10.6743 1.5 10.9329 1.5H13.0671C13.3258 1.5 13.5417 1.69725 13.5651 1.95483L14.2128 9.09592C14.2596 9.61175 14.5046 10.0893 14.8964 10.4281L15.3082 10.7841C15.7475 11.164 16 11.7161 16 12.297V21.5C16 22.0523 15.5523 22.5 15 22.5H9C8.44772 22.5 8 22.0523 8 21.5L8 12.297Z"/>
+            </g>
+          </svg>
+        </a>
+        <a class="social" href="https://instagram.com/AJGeel" target="_blank" aria-label="We sometimes post update stories on Instagram: @AJGeel." data-balloon-pos="down" data-balloon-length="medium">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram">
+            <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+          </svg>
+        </a>
+        <a class="social" href="https://www.github.com/AJGeel/brouwerij-geel" target="_blank" aria-label="Check out the Github repository! (Or don't. I won't tell you what to do.)" data-balloon-pos="down" data-balloon-length="medium">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
+
 
     </main>
 
