4f9fe7dd9c893e2d341ba425bf0003d38e6c543e
Arthur Geel <geelaj01@gmail.com>
2021-11-04 16:36:23 +0100

    Replace scripts.php component

--- recipes/chateau-jaune.php
+++ recipes/chateau-jaune.php
@@ -306,7 +306,21 @@ nav .logo svg {
 
   </main>
 
-  <?php include('../scripts.php'); ?>
+  <!-- Libraries -->
+<script src="../js/modules/swup.min.js" charset="utf-8"></script>
+<script src="../js/modules/headroom.js" charset="utf-8"></script>
+
+<!-- Minified: use in production -->
+<!-- <script src="/js/util.min.js" charset="utf-8"></script> -->
+<script src="../js/util.js" charset="utf-8"></script>
+
+<!-- Main -->
+<!-- <script src="/js/app.min.js" charset="utf-8"></script> -->
+<script src="../js/app.js" charset="utf-8"></script>
+
+<!-- Privacy-oriented & GDPR-compliant analytics -->
+<!-- <script async defer src="https://beampipe.io/js/tracker.js" data-beampipe-domain="brouwerijgeel.nl"></script> -->
+
 
   </body>
 </html>
