9bbef9c8fddd310d1bf9542772be787153a5ccc6
Arthur Geel <geelaj01@gmail.com>
2021-11-04 17:47:47 +0100

    Enhancement: Goodbye php! (#22)
    * WIP: start migrating .php -> .html
    
    * Remove SWUP for now.
    
    * Re-enable swup
    
    * Replace nav.php component
    
    * Replace links.php component
    
    * Replace ascii-biertje.php component
    
    * Replace scripts.php component
    
    * Replace footer.php component
    
    * Replace other-brews.php component
    
    * Replace ascii biertje component
    
    * Replace .php links with .html
    
    * Replace php files with html equivs
    
    * Temp workaround: fix relative image url by duplicating images
    
    * .
    
    * Fix broken css links

--- js/util.js
+++ js/util.js
@@ -18,44 +18,44 @@
 
 // Immutable array of all other brews
 const allOtherBrews = [
-  `<a href="/recipes/ipa-pocalypse.php" class="carrousel--cell">
-    <img class="brew-label" src="/i/labels/ipa-pocalypse.jpg" alt="IPA—Pocalypse label">
+  `<a href="/recipes/ipa-pocalypse.html" class="carrousel--cell">
+    <img class="brew-label" src="i/labels/ipa-pocalypse.jpg" alt="IPA—Pocalypse label">
     <h3>Red Beet IPA &mdash; ABV: 7.4%</h3>
     <p>March 18<sup>th</sup>, 2020</p>
   </a>`,
 
-  `<a href="/recipes/gembier.php" class="carrousel--cell">
-    <img class="brew-label" src="/i/labels/gembier.jpg" alt="Gembier label">
+  `<a href="/recipes/gembier.html" class="carrousel--cell">
+    <img class="brew-label" src="i/labels/gembier.jpg" alt="Gembier label">
     <h3>Ginger Beer  &mdash; ABV: 6.2%</h3>
     <p>February 25<sup>th</sup>, 2020</p>
   </a>`,
 
-  `<a href="/recipes/kiki-cheeky-kriekie.php" class="carrousel--cell">
-    <img class="brew-label" src="/i/labels/kiki-cheeky-kriekie.jpg" alt="Kiki's Cheeky Kriekie label">
+  `<a href="/recipes/kiki-cheeky-kriekie.html" class="carrousel--cell">
+    <img class="brew-label" src="i/labels/kiki-cheeky-kriekie.jpg" alt="Kiki's Cheeky Kriekie label">
     <h3>Cherry Cider &mdash; ABV: 5.9%</h3>
     <p>October 21<sup>th</sup>, 2019</p>
   </a>`,
 
-  `<a href="/recipes/chateau-jaune.php" class="carrousel--cell">
-    <img class="brew-label no-padding" src="/i/labels/chateau-jaune.jpg" alt="Château Jaune label">
+  `<a href="/recipes/chateau-jaune.html" class="carrousel--cell">
+    <img class="brew-label no-padding" src="i/labels/chateau-jaune.jpg" alt="Château Jaune label">
     <h3>Red Wine &mdash; ABV: 7.9%</h3>
     <p>February 28<sup>th</sup>, 2020</p>
   </a>`,
 
-  `<a href="/recipes/toffee-apple.php" class="carrousel--cell">
-    <img class="brew-label" src="/i/labels/toffee-apple.jpg" alt="Toffee Apple Cider label">
+  `<a href="/recipes/toffee-apple.html" class="carrousel--cell">
+    <img class="brew-label" src="i/labels/toffee-apple.jpg" alt="Toffee Apple Cider label">
     <h3>Toffee Cider &mdash; ABV: 4.5%</h3>
     <p>December 7<sup>th</sup>, 2017</p>
   </a>`,
 
-  `<a href="/recipes/dry-hopped-cider.php" class="carrousel--cell">
-    <img class="brew-label" src="/i/labels/dry-hopped-cider.jpg" alt="Dry-Hopped Cider label">
+  `<a href="/recipes/dry-hopped-cider.html" class="carrousel--cell">
+    <img class="brew-label" src="i/labels/dry-hopped-cider.jpg" alt="Dry-Hopped Cider label">
     <h3>Dry-Hopped Cider &mdash; ABV: 5.3%</h3>
     <p>September 18<sup>th</sup>, 2019</p>
   </a>`,
 
-  `<a href="/recipes/hefeweizen.php" class="carrousel--cell">
-    <img class="brew-label no-padding" src="/i/labels/hefeweizen.jpg" alt="Hefeweizen label">
+  `<a href="/recipes/hefeweizen.html" class="carrousel--cell">
+    <img class="brew-label no-padding" src="i/labels/hefeweizen.jpg" alt="Hefeweizen label">
     <h3>Hefeweizen &mdash; ABV: 6.0%</h3>
     <p>April 25<sup>th</sup>, 2020</p>
   </a>`
@@ -71,7 +71,7 @@ function selectRandomBrewUrl() {
 
   // before the url, after the url..
   const pre = '/recipes/';
-  const post = '.php';
+  const post = '.html';
 
   // Concatenate strings to generate a valid URL
   url = `${pre}${url}${post}`;
